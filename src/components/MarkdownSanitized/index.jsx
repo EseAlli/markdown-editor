cb2495fdec973711bc0a5dce857c6bf011fc57e8
EseAlli <essealli.ea@gmail.com>
2022-09-30 09:02:55 +0100

    remove border and added padding to text area, positioned markdown output center

--- src/components/MarkdownSanitized/index.jsx
+++ src/components/MarkdownSanitized/index.jsx
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { markDownContext } from "../../context/markDownContext";
+import "./index.css";
 
 const MarkdownSanitized = () => {
   const { getMarkDownAsHTMLOutput } = useContext(markDownContext);
-  return <div dangerouslySetInnerHTML={getMarkDownAsHTMLOutput()}></div>;
+  return (
+    <div className="markdown-output">
+      <h2>Markdown Output</h2>
+      <div dangerouslySetInnerHTML={getMarkDownAsHTMLOutput()}></div>
+    </div>
+  );
 };
 
 export default MarkdownSanitized;
