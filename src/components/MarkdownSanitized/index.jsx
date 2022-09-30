6007caee37147127c8aa092cc409b6e070e977e6
EseAlli <essealli.ea@gmail.com>
2022-09-30 08:45:03 +0100

    raw input text component styling

--- src/components/MarkdownSanitized/index.jsx
+++ src/components/MarkdownSanitized/index.jsx
@@ -3,7 +3,6 @@ import { markDownContext } from "../../context/markDownContext";
 
 const MarkdownSanitized = () => {
   const { getMarkDownAsHTMLOutput } = useContext(markDownContext);
-  console.log(getMarkDownAsHTMLOutput());
   return <div dangerouslySetInnerHTML={getMarkDownAsHTMLOutput()}></div>;
 };
 
--- src/components/RawInputText/index.jsx
+++ src/components/RawInputText/index.jsx
@@ -1,13 +1,12 @@
 import React, { useContext } from "react";
 import { markDownContext } from "../../context/markDownContext";
-
+import "./index.css";
 const RawInputText = () => {
   const { rawText, handleChangeRawInputedText } = useContext(markDownContext);
   return (
-    <div className="">
-      <h1 className="lg:text-3xl my-3 font-bold">Enter Markdown</h1>
+    <div className="raw-input">
+      <h1>Enter Markdown</h1>
       <textarea
-        className="resize bg-[#E8E8E8]"
         onChange={(e) => handleChangeRawInputedText(e.target.value)}
         value={rawText}
       ></textarea>
